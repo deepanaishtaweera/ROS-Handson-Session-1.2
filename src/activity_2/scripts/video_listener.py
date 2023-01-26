dcc33a8f03338688362a845c845db916f0a4bd16
Deepana Ishtaweera <33963319+deepanaishtaweera@users.noreply.github.com>
2023-01-26 15:23:49 +0530

    Update video_listener.py after test in ROS Noetic

--- src/activity_2/scripts/video_listener.py
+++ src/activity_2/scripts/video_listener.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# OpenCV Tutorial : https://docs.opencv.org/master/d6/d00/tutorial_py_root.html 
+# OpenCV Tutorial : https://docs.opencv.org/master/d6/d00/tutorial_py_root.html
 
 import random
 import rospy
@@ -8,38 +8,40 @@ import numpy as np
 import cv2 as cv
 from sensor_msgs.msg import CompressedImage
 
+
 def callback(dataf):
-    # START CODE HERE
+    # START CODE HERE, TODO: Complete the code
 
     data = None
 
-    np_arr = None # Convert string to uint8 using np.fromstring()
-    
-    image = cv.imdecode(None, cv.IMREAD_COLOR) # Fill the None
-    
-    cv.imshow('listener',None) # Display the image
-    
+    # TODO: Convert string to uint8 using np.fromstring()
+    np_arr = None
+
+    image = cv.imdecode(None, cv.IMREAD_COLOR)  # TODO: Fill the None
+
+    cv.imshow('listener', None)  # TODO: Display the image
+
     cv.waitKey(1)
 
     # END CODE HERE
 
+
 def listner():
     rospy.loginfo("I'm listening ...")
-    rospy.init_node('listener',anonymous=True)
+    rospy.init_node('listener', anonymous=True)
 
     # START CODE HERE
 
-    # Create a subscriber to topic '/camera/feed'
-    rospy.Subscriber(None,None,None)
+    # TODO: Create a subscriber to topic '/camera/image_raw/compressed'
+
 
     # END CODE HERE
 
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
         listner()
     except rospy.ROSInterruptException:
         pass
-
-
