ba754352d6238f0c629f0040d0386c5c888db608
Deepana Ishtaweera <33963319+deepanaishtaweera@users.noreply.github.com>
2023-01-26 15:13:52 +0530

    Update video_talker.py after test with ROS Noetic

--- src/activity_2/scripts/video_talker.py
+++ src/activity_2/scripts/video_talker.py
@@ -1,47 +1,51 @@
 #!/usr/bin/env python
 
-# OpenCV Tutorial : https://docs.opencv.org/master/d6/d00/tutorial_py_root.html 
+# OpenCV Tutorial : https://docs.opencv.org/master/d6/d00/tutorial_py_root.html
 
-import random
-import sys,os
 import rospy
 import numpy as np
 from sensor_msgs.msg import CompressedImage
 import cv2 as cv
-from time import sleep
 
 
 def talker():
-
     # START CODE HERE
 
-    pub = None # Publisher to topic /camera/feed
+    # TODO: Create publisher to topic /camera/image_raw/compressed
+    pub = None
 
-    rospy.init_node('talker_node',anonymous=True)
-    
-    cap = None # Read the video in videos folder
+    rospy.init_node('talker_node', anonymous=True)
 
-    fps = None # Get the FPS Value - Hint : use cv.cap.get()
-    
-    rate = rospy.Rate(None) # Fill the None
-    
+    # TODO: Read the video in videos folder - Hint: use absolute path using forward slashes
+    cap = None
+
+    # Check if video file is opened successfully
+    if (cap.isOpened() == False):
+        rospy.loginfo("Error opening video file")
+
+    # TODO: Get the FPS Value - Hint : use cv.cap.get()
+    fps = None
+
+    rate = rospy.Rate(None)  # TODO: Fill the None
+
     while not rospy.is_shutdown() and cap.isOpened():
-        
-        msg = None # Create a msg
-        
-        ret, frame = None # Read the cap
-        
-        if ret==False:
-            break  
+
+        msg = None  # TODO: Create a msg
+
+        ret, frame = None  # TODO: Read the cap
+
+        if not ret:
+            break
         msg.header.stamp = rospy.Time.now()
         msg.format = "jpeg"
         msg.data = np.array(cv.imencode('.jpg', frame)[1]).tostring()
         pub.publish(msg)
         rate.sleep()
-    
+
     cap.release()
-    
+
     # END CODE HERE
+
 
 if __name__ == '__main__':
     try:
